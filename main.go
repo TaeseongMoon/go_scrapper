7771ff491242c1b59e51754e5bce1ee4208e624d
TeaseongMoon <ansxotjd12@naver.com>
2020-03-08 00:46:02 +0900

    add go and server

--- main.go
+++ main.go
@@ -1,77 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
+	"os"
+	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/learngo/scrapper"
+
+	"github.com/labstack/echo"
 )
 
-type extractedJob struct {
-	id       string
-	title    string
-	location string
-	salary   string
-	summary  string
+// FileName for save scv
+const FileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
 }
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
-
-func getPage(page int) {
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-
-	res, err := http.Get(pageURL)
-	checkErr(err)
-	checkCode(res)
-
-	defer res.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-
-	searchCards := doc.Find(".jobsearch-SerpJobCard")
-
-	searchCards.Each(func(i int, card *goquery.Selection) {
-		id, _ := card.Attr("data-jk")
-		fmt.Println(id)
-		title := card.Find(".title>a").Text()
-		fmt.Println(title)
-	})
-}
-
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseURL)
-	checkErr(err)
-	checkCode(res)
-
-	defer res.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		pages = s.Find("a").Length()
-	})
-	return pages
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("Request faild with Status : ", res.StatusCode)
-	}
+func handleScrape(c echo.Context) error {
+	defer os.Remove(FileName)
+	term := strings.ToLower(scrapper.CleanStrings(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(FileName, FileName)
 }
 
 func main() {
-	totalPages := getPages()
-	for i := 0; i < totalPages; i++ {
-		getPage(i)
-	}
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
